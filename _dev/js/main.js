83ce0496052ec582776bb64cc7a6a582da81047d
ponomarev-iv <ponomarev-iv@galagodigital.ru>
2017-07-11 15:10:52 +0500

    Доделал главную страницу

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -22,7 +22,25 @@ function swiperInit() {
     })
 }
 
+function initPopup(){
+    $('.js-popup-img').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
+}
+
 $(document).ready(function(){
     mobileMenu();
     swiperInit();
+    initPopup();
 });
